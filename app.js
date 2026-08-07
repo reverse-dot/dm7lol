c4c2c5060bf222823f9af2e82708f68e63655294
Leandro Lopez <cobblepotdesign@gmail.com>
2026-08-07 04:34:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ function renderPodium() {
         </span>
       </div>
       <div class="flex justify-between gap-2 mb-3">
-        <div class="text-[13px]"><b class="block text-[18px] font-extrabold">${p.wins}W ${p.losses}L</b><span class="text-text-dim text-xs">${p.wins+p.losses} partidas</span></div>
+        <div class="text-[13px]"><b class="block text-[18px] font-extrabold">${p.wins}W ${p.losses}L</b><span class="text-text-dim text-xs italic">${p.wins+p.losses} partidas</span></div>
         <div class="text-[13px]"><b class="block text-[18px] font-extrabold">${wr}%</b><span class="text-text-dim text-xs italic">Winrate</span></div>
         <div class="text-[13px]"><b class="block text-[18px] font-extrabold">${countWL(p.last20,1)}W ${countWL(p.last20,0)}L</b><span class="text-text-dim text-xs italic">Últimas 20</span></div>
       </div>
